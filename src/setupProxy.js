903cc86eab87c85e61b929147ff101d13f653263
HobumPark <gloryofalive@naver.com>
2023-01-30 20:37:53 +0900

    twentyone commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default (app) => {
+module.exports = (app) => {
 	app.use(
 		createProxyMiddleware('/board', {
 			target: 'http://mynodetest.cafe24app.com', 
